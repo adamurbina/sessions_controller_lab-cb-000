9f59de0fda260dc99c3035deb05742cd0393dd2b
Adam Urbina <adamurbina@gmail.com>
2018-11-13 00:27:17 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   def create
     if session[:name].nil? || session[:name].empty?
       redirect_to '/login'
+    else
+      session[:name] = params[:name]
     end
   end
 
