79fd33212719fe090bca92dfd1f6c2139da58c37
Adam Urbina <adamurbina@gmail.com>
2018-11-13 01:01:37 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:name].clear
+    session[:name].clear unless session[:name].nil?
   end
 
   def require_login
