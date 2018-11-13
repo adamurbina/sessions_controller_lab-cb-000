5b36de9288d0e3104ddb60ced2628239a2eb74c8
Adam Urbina <adamurbina@gmail.com>
2018-11-13 00:56:11 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
-  before_action :require_login
-  skip_before_action :require_login, only: [:create]
+  # before_action :require_login
+  # skip_before_action :require_login, only: [:create]
 
 
   def create
